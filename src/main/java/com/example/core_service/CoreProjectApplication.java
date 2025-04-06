53cf8eb909621a097dd211f7e1507ed7ddbd7a11
Besogon1238 <tarancoll429@gmail.com>
2025-04-06 14:18:46 +0400

    Initial commit

---
+++ src/main/java/com/example/core_service/CoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreProjectApplication.class, args);
+    }
+
+}
